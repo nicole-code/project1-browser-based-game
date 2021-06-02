f15adcd9649e70d4a36cb1d4f90a243817275609
Nicole Stawikowski <nicole.stawikowski@gmail.com>
2021-06-02 14:27:50 -0400

    updates again

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let arrayOfMovies = ["Overboard", "Ghostbusters", "Spaceballs" , "Heathers"]
 
 let currentWord = "";
 
-let currentScore = 0 
+let wrongGuesses = 0; 
 
-let usersGuess = ["", "", "", ]; //keeps track of what letters user has guessed so far
+let correctGuesses = 0;
+
 
 /*----- cached element references -----*/
 
@@ -29,6 +30,7 @@ for (let btn of btns) {
 function init () {
     let randomNumber = Math.floor(Math.random() * 4);
     currentWord = arrayOfMovies[randomNumber]; //this assigns the value of currentWord to the array of movies at a randon number
+    console.log(currentWord)
     generateSpace ();
 }
 
@@ -42,12 +44,17 @@ function buttonClick (event) {
 
     if (wordIncludesLetter) {
         document.querySelectorAll(".letter-" + letterClickedLowerCase).forEach(function(el){ 
-            el.innerText = letterClicked;  
+            el.innerText = letterClicked; 
+            correctGuesses += 1; 
+            console.log(correctGuesses)
         }) 
     } else {
-       currentScore += 1 //adding one to current score
-       document.getElementById("userScore").innerText = currentScore
-    }  event.target.disabled = true; 
+       wrongGuesses += 1 //adding one to current score
+       document.getElementById("userScore").innerText = wrongGuesses
+    }  
+    
+    event.target.disabled = true; 
+    checkWinOrLose();
 }
 
 
@@ -62,28 +69,29 @@ function generateSpace () {
 }
 
 
-function winOrLose () {
-    let winningImage = document.createElement("IMG");
-    winningImage.src = "images/You've been Slimmmed!.png"; //refer to proper image
-    let losingImage = document.createElement("IMG");
-    losingImage.src = "images/80s YOU WIN.png"; 
-        if (currentWord ===  ) { 
+function checkWinOrLose () {
+    // let winningImage = document.createElement("IMG");
+    // winningImage.src = "images/You've been Slimmmed!.png"; //refer to proper image
+    // let losingImage = document.createElement("IMG");
+    // losingImage.src = "images/80s YOU WIN.png"; 
+        if (currentWord.length === correctGuesses) { 
             alert("you won")
             //display winning image
-            theBody.appendChild(winningImage);
+            // theBody.appendChild(winningImage);
             
         } 
 
-        else if (currentScore === 4){
+        else if (wrongGuesses === 4){
             alert("you lost")
               //display the losingImage   
-            theBody.appendChild(losingImage);     
+            // theBody.appendChild(losingImage);     
         } 
         
         else {
 
         }
 }
+
 
 
 
